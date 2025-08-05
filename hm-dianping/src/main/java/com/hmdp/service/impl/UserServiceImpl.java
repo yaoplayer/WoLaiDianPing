bb4ab7ceb9ae86d426d71456fde56c400d8085a9
yao <1316580188@qq.com>
2025-08-05 15:52:04 +0800

    基础代码和短信验证码发送代码

---
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+        String code = RandomUtil.randomNumbers(6);
+
+        session.setAttribute("code", code);
+
+        log.debug("发送短信验证码成功，验证码；{}",code);
+
+        return Result.ok();
+    }
+}
